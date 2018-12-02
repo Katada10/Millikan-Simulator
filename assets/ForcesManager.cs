cea51a6615a4f5b62f8078638642d5791dffd5df
Katada Freije <katadafreije@gmail.com>
2018-12-02 17:18:06 -0400

    Shows multiples of elementary charge, still needs variable chaning

---
+++ assets/ForcesManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcesManager : MonoBehaviour {
+    private float radiusTop;
+    private GameObject oilDrop;
+    private Rigidbody rb;
+
+    public float particleSize;
+    public GameObject topPlate;
+    public GameObject bottomPlate;
+
+    //Kg/m^3
+    private readonly float density = 930f;
+    private readonly float voltage = 700;
+
+    private void CalculateForces(Rigidbody rb)
+    {
+        var radius = oilDrop.GetComponent<SphereCollider>().radius / 1000;
+        var mass = density * 4 * Mathf.PI * Mathf.Pow(radius, 3);
+        rb.mass = particleSize;
+        var factor = rb.mass / mass;
+
+        var gravity = rb.mass * -10f;
+        rb.AddForce(new Vector3(0, gravity, 0));
+
+        var distance = Mathf.Abs(bottomPlate.transform.position.y - topPlate.transform.position.y);
+
+        //DONT DELETE
+        var charge = particleSize * (1.6f * Mathf.Pow(10,-9)) * factor;
+        var factoredCharge = charge / factor / Mathf.Pow(10, 10);
+
+        rb.AddForce(new Vector3(0, (voltage * charge) / distance, 0));
+
+        Debug.Log(factoredCharge);
+    }
+
+    void FixedUpdate()
+    {
+        oilDrop = GameObject.FindGameObjectWithTag("Oil");
+        if (oilDrop != null)
+        {
+            radiusTop = oilDrop.transform.position.y - topPlate.transform.position.y;
+
+            rb = oilDrop.GetComponent<Rigidbody>();
+            CalculateForces(rb);
+        }
+    }
+}
---
+++ assets/OilDropper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilDropper : MonoBehaviour {
+    private ParticleSystem system;
+    private List<ParticleCollisionEvent> events;
+
+    public Transform position;
+    public Transform oilPrefab;
+
+
+	private bool oilDropped;
+
+    void Start()
+    {
+        system = GetComponent<ParticleSystem>();
+        events = new List<ParticleCollisionEvent>();
+        oilDropped = false;
+    }
+
+	void OnParticleCollision(GameObject obj)
+	{
+		if(obj.name == "Oil Drop Trigger")
+        {
+            obj.GetComponent<Collider>().enabled = false;
+            if(!oilDropped)
+            {
+                oilDropped = true;
+                Instantiate(oilPrefab, position.transform.position,  Quaternion.identity);
+            }
+        }
+	}
+}
